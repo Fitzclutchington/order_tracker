1fa1b81953d9bf35d4f6a9fd02dc9680b8624ab2
Andrew Fitzgerald <fitzgeralda2010@gmail.com>
2024-07-05 23:02:49 -0400

    create items data model

---
+++ order_tracker/models/items.py
@@ -0,0 +1,44 @@
+from sqlalchemy import (
+    Boolean,
+    Column,
+    DateTime,
+    Enum,
+    ForeignKey,
+    Integer,
+    String,
+    func,
+    Float
+)
+
+from order_tracker.database.database import Base
+
+
+class Manufacturer(Base):
+    __tablename__ = "manufacturers"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+
+
+class Items(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), nullable=False)
+    manufacturer_id = Column(Integer, ForeignKey("manufacturers.id"))
+    model_name = Column(String)
+    series = Column(String)
+    url = Column(String)
+    security_level = Column(String)
+    height_inside = Column(Integer)
+    height_outside = Column(Integer)
+    width_inside = Column(Integer)
+    width_outside = Column(Integer)
+    depth_inside = Column(Integer)
+    depth_outside = Column(Integer)
+    overall_depth = Column(Integer)
+    capacity = Column(Float)
+    fire_protection = Column(String)
+    weight = Column(Integer)
+    price = Column(Float)
+    currency = Column(String)
+    swing = Column(String)
